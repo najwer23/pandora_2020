29aa0079fd9afbc48716dd311caddd6abe3c0e2c
Travis CI <najwer23@gmail.com>
2020-04-19 17:11:05 +0200

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace pandora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Press Enter to exit...");
+            Console.ReadLine();
+        }
+    }
+}
